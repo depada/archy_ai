84da0f795ffe156c0849706bddd43cd4ca707d7b
Pavan <kolipaka.pavan.kp@gmail.com>
2026-04-22 01:02:41 +0530

    link title

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -8,8 +8,8 @@ import { createProject, getProjects } from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Archy AI" },
+    { name: "description", content: "Welcome to Archy AI!" },
   ];
 }
 
